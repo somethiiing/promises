a0bd218b02b05fe72bac224654573181b4b5b668
Brandon Courtney <brandon.h.courtney@gmail.com>
2016-03-23 14:35:40 -0700

    finished first file

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -16,14 +16,14 @@ var request = require('request');
 // Notice that the callback we pass into it meets the two conditions above
 // Uncomment the lines below and run the example with `node exercises/bare_minimum/callbackReview.js`
 
-// fs.readFile(__dirname + '/../../README.md', 'utf8', function (err, content) {
-//   console.log('Example from callbackReview.js')
-//   if (err) { 
-//     console.log('fs.readFile failed :(\n', err)
-//   } else {
-//     console.log('fs.readFile successfully completed :)\n', content)
-//   }
-// });
+fs.readFile(__dirname + '/../../README.md', 'utf8', function (err, content) {
+  console.log('Example from callbackReview.js')
+  if (err) { 
+    console.log('fs.readFile failed :(\n', err)
+  } else {
+    console.log('fs.readFile successfully completed :)\n', content)
+  }
+});
 
 
 /******************************************************************
@@ -36,17 +36,36 @@ var request = require('request');
 // This function should retrieve the first line of the file at `filePath`
 // HINT: Passing 'utf8' as the second argument to fs.readFile will give you a stringified file
 // HINT: You can get an array of lines by splitting on the '\n' character
-var pluckFirstLineFromFile = function (filePath
-) {
-  // YOUR CODE HERE
+var pluckFirstLineFromFile = function (filePath, callback) {
+	fs.readFile(filePath, 'utf8', function(err, content) {
+		if (err) {
+			callback(err);
+		} else {
+			content = content.split('\n');
+			callback(null, content[0]);
+		}
+	});
 };
 
 // This function should retrieve the status code of a GET request to `url`
 // HINT: the `request` module has been included to help you send HTTP requests
 // HINT: there is a `statusCode` property on the `response` object
-var getStatusCode = function (url
-) {
-  // YOUR CODE HERE
+
+// var request = require('request');
+// request('http://www.google.com', function (error, response, body) {
+//   if (!error && response.statusCode == 200) {
+//     console.log(body) // Show the HTML for the Google homepage. 
+//   }
+// })
+
+var getStatusCode = function (url, callback) {
+  request(url, function(error, response, body) {
+  	if (error) {
+  		callback(error);
+  	} else {
+  		callback(null, response.statusCode);
+  	}
+  });
 };
 
 // Export these functions so we can unit test them
--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -28,6 +28,9 @@ var Promise = require('bluebird');
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function (filePath) {
   // TODO
+  return new Promise(function(resolve, reject) {
+
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
